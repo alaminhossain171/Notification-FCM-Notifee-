238a70727189deab38c57c6834cfe4d6e6f1e4b1
alaminhossain171 <alamin.cse.171@gmail.com>
2023-04-08 22:00:10 +0600

    server key removed

--- index.js
+++ index.js
@@ -18,6 +18,5 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
 backgroundEvents();
 
 
-// server key 
-// AAAA7c6OzpM:APA91bHJtN6y71J9Jc1jWFVHq2os8uPB0Hh-2bd6UT9_Q0HKNKqjfYSz-qmK-rGUmIxrdGkQh-VgfBDoX9ms3wikAyDOsw0FmT9AXsdabNIzjZrVEMCqdclOJ5guZsbzt6ufC0EGuFRE
+
 AppRegistry.registerComponent(appName, () => App);
